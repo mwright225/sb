1a5539e142ee5c5a0c2a0fb430b9ec07d8b7bbdc
mwright225 <147872245+mwright225@users.noreply.github.com>
2023-10-13 19:16:56 -0400

    Add files via upload

--- sb.js
+++ sb.js
@@ -6,18 +6,25 @@ function nInc(dir='up') {
     if (1 == currNum) { return; }
     currNum--;
   }
-  let div = document.getElementById('ttl');
-  div.innerHTML = `SB ${currNum} <small>(${currNum + 1965}-${currNum + 1966})</small>`;
-  console.log(currNum)
-  test(currNum)
+    
+  makePlayoffs(currNum)
 }
-function test(yr) {  
-  let yrData = playoff[yr];
+function makePlayoffs(yr) {  
+
+  let yrData = JSON.parse(JSON.stringify(playoff[yr]));
   if (!yrData) { return; }
+  
+  let ttldiv = document.getElementById('ttl');
+  ttldiv.innerHTML = `SB ${yr} <small>(${yr + 1965}-${yr + 1966})</small>`;
+
+    
+  yrData.SB = {
+    SB: [yrData.SB]
+  }
   
   let h = '<div class="poWrap">';
   Object.keys(yrData).map(conf => {
-    if (!yrData?.[conf]) { return; }
+    if (!yrData?.[conf]) { return; }    
     h += `<div class="confWrap ${conf}">`;
     Object.keys(yrData[conf]).map(poRound => {
       if (!yrData?.[conf]?.[poRound]?.map) { return; }
@@ -38,25 +45,25 @@ function test(yr) {
   
   let div = document.getElementById('mainDiv');
   div.innerHTML = h;
-  
-  
+      
+  //editJson(currNum)
 }
 
 function makeGame(game) {
-  console.log(game)
+  
   let h = `
   <div class="gameWrap">
   
     <div class="teamContainer">
     
       <div class="teamWrap">     
-        <div class="teamSeed">${game.wSeed}</div>
-        <div class="teamIcon"><image class='poImg' src='img/${game.wID}.png' /></div>
-        <div class="teamRecord">${game.wRecord}</div>
+        <div class="teamSeed">${game?.wSeed}</div>
+        <div class="teamIcon"><image alt='?' class='poImg' src='img/${game?.wID}.png' /></div>
+        <div class="teamRecord">${game?.wRecord}</div>
       </div>
          
       <div class="scoreWrap">  
-        ${game.wScore}   
+        ${game?.wScore}   
       </div>
     
     </div>
@@ -64,13 +71,13 @@ function makeGame(game) {
     <div class="teamContainer">
     
       <div class="teamWrap">     
-        <div class="teamSeed">${game.lSeed}</div>
-        <div class="teamIcon"><image class='poImg' src='img/${game.lID}.png' /></div>
-        <div class="teamRecord">${game.lRecord}</div>
+        <div class="teamSeed">${game?.lSeed}</div>
+        <div class="teamIcon"><image class='poImg' src='img/${game?.lID}.png' /></div>
+        <div class="teamRecord">${game?.lRecord}</div>
       </div>
          
       <div class="scoreWrap">  
-        ${game.lScore}   
+        ${game?.lScore}   
       </div>
     
     </div>
@@ -80,6 +87,64 @@ function makeGame(game) {
  return h;
   
 }
-function editJson() {
+function editJson(yr) {
+  let yrData = JSON.parse(JSON.stringify(playoff[yr]));
+  if (!yrData) { return; }
+    
+  yrData.SB = {
+    SB: [yrData.SB]
+  }
+  
+  let seeding = {
+    AFC: {},
+    NFC: {}
+  }
+  let rounds = {}
+               
+  Object.keys(yrData).map(conf => {
+    if (!yrData?.[conf]) { return; }    
+    if (!rounds[conf]) { rounds[conf] = {}; }    
+    
+    Object.keys(yrData[conf]).map(poRound => {
+      if (!yrData?.[conf]?.[poRound]?.map) { return; }
+      
+      if (!rounds[conf][poRound]) { rounds[conf][poRound] = []; }
+      
+      yrData[conf][poRound].map(game => {
+      
+        rounds[conf][poRound].push({
+          wID: game?.wID,
+          wScore: game?.wScore,
+          lID: game?.lID,
+          lScore: game?.lScore,
+        })
+        
+        if (['AFC','NFC'].includes(conf)) {        
+          ['w','l'].map(t => {
+            if (!game[`${t}Seed`]) { return; }
+            let thisSeed = game[`${t}Seed`];
+            if (!seeding[conf][thisSeed]) {
+              seeding[conf][thisSeed] = {
+                Team: game[`${t}Team`],
+                ID: game[`${t}ID`],
+                Record: game[`${t}Record`]
+              }
+            }
+          })
+        }
+        
+      })
+     
+    })        
+   
+  })
+  
+  //console.log(seeding)
+  //console.log(rounds)
+  playoff2[yr+''] = {
+    seeding: seeding,
+    rounds: rounds
+  }
+ 
 }
 
